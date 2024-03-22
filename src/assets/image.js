19e7ddd79a74e13e03d9edd71ae836b75fe4bb00
onyedikachi okonkwo <okonkwovincent@gmail.com>
2024-03-22 14:47:42 +0100

    added

---
+++ src/assets/image.js
@@ -0,0 +1,35 @@
+import history from "./history.png"
+import menu_icon from "./menu.png"
+import bulb_icon from "./bulb light.webp"
+import code_icon from "./code.png"
+import compass_icon from "./compass.png"
+import gallery_icon from "./gallery_icon.png"
+import gallery_icon2 from "./gallery_icon2.png"
+import gemini_icon from "./gemini.jpg"
+import john_icon from "./john.png"
+import message_icon from "./message.png"
+import mic_icon from "./mic_icon.jpg"
+import plus_icon from "./plus"
+import Quetions_icon from "./Question.png"
+import send_icon from "./send_icon.png"
+import youtube_icon from "./youtube_icon.png"
+
+
+
+export const assests = {
+    history,
+    menu_icon,
+    bulb_icon,
+    code_icon,
+    compass_icon,
+    gallery_icon,
+    gallery_icon2,
+    gemini_icon,
+    john_icon,
+    message_icon,
+    mic_icon,
+    plus_icon,
+    Quetions_icon,
+    send_icon,
+    youtube_icon,
+}
